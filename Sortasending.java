c5c9a7fbec80b86d9778d08e5d33c1bdfa411790
Amrit478 Dhillon <Amritraj4786@gmail.com>
2024-10-11 23:47:38 +0000

    Another

---
+++ Sortasending.java
@@ -0,0 +1,7 @@
+public class Sortasending {
+    /*Modify the function to merge the arrays in descending order.*/
+    public static void main(String[] args) {
+        int arr1[] = { 1, 3, 4, 5 };
+        int arr2[] = { 2, 4, 6, 8 };
+    }
+}
