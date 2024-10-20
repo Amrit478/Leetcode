fcb5349404c3cc2e2ac356012d6bac62540e6fc0
Amrit478 Dhillon <Amritraj4786@gmail.com>
2024-10-20 11:51:39 +0000

    New directory

---
+++ Linkedlist/StartInteger.java
@@ -0,0 +1,19 @@
+package Linkedlist;
+
+import java.util.LinkedList;
+import java.util.Scanner;
+
+public class StartInteger {
+    public static void main(String[] args) {
+    //I am going to work integer linked list here
+    Scanner input = new Scanner(System.in);
+    System.out.println("Enter the numbers when finish press 0");
+
+    LinkedList <Integer> list = new LinkedList<>();
+    try {
+        
+    } catch (Exception e) {
+    
+    }   
+}
+}
---
+++ Linkedlist/Stsrtstring.java
@@ -0,0 +1,18 @@
+package Linkedlist;
+import java.util.LinkedList;
+import java.util.Scanner;
+
+public class Stsrtstring {
+    public static void main(String[] args) {
+    //I am going to work integer linked list here
+    Scanner input = new Scanner(System.in);
+    System.out.println("Enter the numbers when finish press 0");
+
+    LinkedList <String> words = new LinkedList<>();
+    try {
+        
+    } catch (Exception e) {
+    
+    }   
+}
+}
