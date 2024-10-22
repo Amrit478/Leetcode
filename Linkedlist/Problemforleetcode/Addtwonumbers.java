3b013743b069cd26900a4c71aa2a106b524069d4
Amrit478 Dhillon <Amritraj4786@gmail.com>
2024-10-22 20:39:02 +0000

    need to make change

---
+++ Linkedlist/Problemforleetcode/Addtwonumbers.java
@@ -0,0 +1,16 @@
+package Linkedlist.Problemforleetcode;
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+public class Addtwonumbers {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        
+    }
+}
