ad32efc33dd5a3400690b4c7aff17af8c480b4fc
Amrit478 Dhillon <Amritraj4786@gmail.com>
2024-10-24 11:33:59 +0000

    Writing generic class and data type class in Linked List

--- Linkedlist/Singlylinkedlist.java
+++ Linkedlist/Singlylinkedlist.java
@@ -1,7 +1,30 @@
 package Linkedlist;
 
-public class Singlylinkedlist{
+import javafx.scene.Node;
+
+public class Singlylinkedlist extends Node{
     public static void main(String[] args) {
         
     }
 }
+//This is my generic class which can accept any data type including arrays of int, double or strings
+class Node<T>{
+    T data;
+    private Node next;
+
+    public Node(T data){
+        this.data = data;
+        this.next = null;
+    }
+}
+
+//Rigid class which can accept only certain data types
+/*class Node{
+    private int data;
+    private Node next;
+
+    public Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+} */
