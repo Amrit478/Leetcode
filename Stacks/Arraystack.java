01499ce660693e3991024d6408fd8c40174bf14b
AMRITRAJ S DHILLON <123419466+Amrit478@users.noreply.github.com>
2025-02-13 12:52:26 -0500

    Create Arraystack.java

---
+++ Stacks/Arraystack.java
@@ -0,0 +1,18 @@
+/*I will put stacks into arrays and than design the stacks with the 
+coding with array*/
+public class ArrayStack {
+  //I will put attribute and constructor here
+  private int top;
+  private int[] arr;
+
+  public ArrayStack(int capacity) {
+    arr = new int[capacity];
+    top = -1;
+  }
+  /**/
+  
+
+
+
+/**/
+/**/
