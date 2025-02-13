d0cd28459422ecbf8c32e8fb2b44d2ded50ce190
AMRITRAJ S DHILLON <123419466+Amrit478@users.noreply.github.com>
2025-02-13 13:08:45 -0500

    Update Arraystack.java

--- Stacks/Arraystack.java
+++ Stacks/Arraystack.java
@@ -9,10 +9,28 @@ public class ArrayStack {
     arr = new int[capacity];
     top = -1;
   }
-  /**/
-  
-
-
-
-/**/
-/**/
+  /*Add new element to the end of the array*/
+  public void push(int value) {
+    arr[++top] = value;
+  }
+/*Remove the last element from the array and return its value*/
+  public int pop() throws Exception {
+    if(top < 0) {
+      throws new Exception;
+    }
+    int value = arr[top];
+    top--;
+    return value;
+  }
+  /*Get the top element of the array*/
+  public int peek() throws Exception {
+    if(top < 0) {
+      throws new Exception; 
+    }
+    return arr[top];
+  }
+    /*Check if the list is empty or not*/
+    public boolean isEmpty() {
+      return top < 0;
+    }
+}
