3b85d5c44cd4831d2f419264cdb569df6ef024db
AMRITRAJ S DHILLON <123419466+Amrit478@users.noreply.github.com>
2025-02-13 07:08:52 -0500

    Create Work.java
    Need to work on main class and operations class and this is just a starting point for me

---
+++ Linkedlist/Work.java
@@ -0,0 +1,42 @@
+public class Work {
+  public static void main(String[] args) {
+//I will working on the coding where I will learning and trying to typing code from the websites
+    //I will be making changes in this class when I reach home and try all to make work 
+    
+  }
+}
+
+//This class can be extended by another class but this class will get all the functions from main class however the other class will do the solutions
+    public class DoublyLinkedlist {
+      //I can create a integer, string or even a generic class which will accept both integers and strings as well
+      //Attributes are here
+        private int data;
+        private Node next;
+        private Node last;
+
+//I will write a constructor for this class here
+      public DoublyLinkedList(int data) {
+        this.data = data;
+        this.next = null;
+        this.last = null;
+}
+}
+
+//This class will implement the solutions which I will be sending through main class using Doublylinkedlist class
+
+  public class Operations extends Doublylinkedlist {
+    //Another important functions is that class uses extends and interface use implement in the function
+    //Attributes will be getting from DoublylinkedList here including constructor which will make me use super constructor here and almost it is called using inheritance
+    public Operations(int data)
+    //Called a Super class constructor here
+      super(data, next, last);
+    
+    // Will be writing operations here for my class
+
+
+    
+    
+
+
+
+    
