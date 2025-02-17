d63540e83a7b28614766154e4e4dbeed26de9710
AMRITRAJ S DHILLON <123419466+Amrit478@users.noreply.github.com>
2025-02-17 07:19:15 -0500

    Create Mergearray.java

---
+++ Arrays/Mergearray.java
@@ -0,0 +1,28 @@
+/*Question 
+
+Given two sorted arrays, the task is to merge them in a sorted manner.
+Examples: 
+
+Input: arr1[] = { 1, 3, 4, 5}, arr2[] = {2, 4, 6, 8} 
+Output: arr3[] = {1, 2, 3, 4, 4, 5, 6, 8}*/
+
+/*Naive approach to put both of these together and sort them using sorting method and the o(n) time be square 
+
+However I will be solving it using optimization method where I will be solving it using merge sort and now I will try to solve it*/
+
+//Main method where it will be solved
+
+//I will compare both array and than decide which element should go first based on that
+
+
+
+
+
+
+
+public class Mergearray {
+  //Need to write when reach home 
+psvm{
+  
+}
+}
