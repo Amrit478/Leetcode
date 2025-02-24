fba19631c4103e249275c54c5eca15d7e8f2b77a
AMRITRAJ S DHILLON <123419466+Amrit478@users.noreply.github.com>
2025-02-24 09:38:50 -0500

    Update miniormaxarray.java

--- Arrays/Question/miniormaxarray.java
+++ Arrays/Question/miniormaxarray.java
@@ -1,1 +1,14 @@
+public class miniormaxarray {
+  //Input x[] = [4,2,3,4,5,0] max = 5 min = 0
+/*I am going to use merge sorting where the first will be smallest number and last will be the biggest number
+and I will solve using two pointers to solve this.*/
+public static void findMinMax(int x[], int n) {
+  //Two pointers which will return the min and max pointers 
+   int min = 0;
+   int max = 0;
+  Hashmap<Integer> amrit = new Hashmap<>();
 
+  for(int i = 0; i < x.length; i++) {
+    ///write logic here
+  }
+}
