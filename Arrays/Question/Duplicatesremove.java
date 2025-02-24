327b28d60c934446765a474e6d03b910ed0dfe3f
AMRITRAJ S DHILLON <123419466+Amrit478@users.noreply.github.com>
2025-02-24 09:45:07 -0500

    Create Duplicatesremove.java

---
+++ Arrays/Question/Duplicatesremove.java
@@ -0,0 +1,13 @@
+
+/*Input: nums = [1,1,2]
+Output: 2, nums = [1,2]*/
+
+public int Duplicatesremove(int [] nums) {
+  /*I will use hashmap for remove the duplicates*/
+  Hashmap<Integer> amrit = new Hashmap<>();
+int i = 0;
+  while(i < nums.length) {
+    //if it duplicate than remove otherwise return the array using hashmap which should return the unique numbers only
+    i++;
+  }
+}
