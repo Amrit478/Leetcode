3b6a95a3ee8e8b7c3e56f0632818a4db4c5a9636
AMRITRAJ S DHILLON <123419466+Amrit478@users.noreply.github.com>
2025-03-09 20:23:44 -0400

    Create newLinked.java

---
+++ Linkedlist/newLinked.java
@@ -0,0 +1,18 @@
+/*I am creating a new list where I will converting a linked list on java from an array and I will be creating a new class*/
+Class Node {
+  private int data;
+  private Node next;
+  private Node previous;
+  public Node(int data) {
+    this.data = data;
+    this.next = null;
+    this.previous = null;
+  }
+}
+
+
+//Here I will declare the main method where it will send the data to other class and It will work on  a function
+public static void(String[] args) {
+  //Will be sending array from here to the linked list class
+  
+} 
