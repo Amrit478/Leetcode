bd7f2449b53e9090713d46d0daf40f61d77d43e1
AMRITRAJ S DHILLON <123419466+Amrit478@users.noreply.github.com>
2025-03-10 07:12:16 -0400

    Create SingleStack.java

---
+++ Stacks/SingleStack.java
@@ -0,0 +1,21 @@
+public class LinkedStack implements Stack {
+  private record Node(Object node, Node next)
+}
+private Node top = null;
+private int size = 0;
+
+//I have to write a method for push
+
+
+//I have to write a method for pop
+
+
+//I have to write a method for isEmpty
+
+
+
+//I have to write a method for peek
+
+
+
+//I have to write a method for size
