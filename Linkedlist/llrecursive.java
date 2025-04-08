3844c7bb1078ac6f1655fcd88d2b562a86772ab1
AMRITRAJ S DHILLON <123419466+Amrit478@users.noreply.github.com>
2025-04-08 06:57:46 -0400

    Create llrecursive.java

---
+++ Linkedlist/llrecursive.java
@@ -0,0 +1,22 @@
+/*This is other version of linked list where I will write linked list functions as recursive */
+
+/*Note - when we are applying functions recursively than we are not not changing the head but we copy head data into stack memory. after copying that data into stack memory than we call that stack
+memory again and again which makes our head data stays the same. when we are calling the function each time recursively we are calling the stack function not the head function.
+
+ALGORITHM STEPS THAT HOW WE HAVE TO IMPLEMENT RECURSION
+1. In method we have given the head of the function.
+2. We have design base case for the recursion or some people call when the method will end
+3. than we have to call the hypothesis of the function which is how itself calls the function and that will depend on the requirement.
+*/
+
+
+//This is my void method where I will be printing the head function as recursively
+
+
+//This is my node method where I will be adding the node to the head function as recursively
+
+
+//This is my another node method where I will be deleting the tail node from the function as recursively
+
+
+and so on 
