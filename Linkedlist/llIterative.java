f6bd72dd2359bd532023f95036e27e987ecb0c99
AMRITRAJ S DHILLON <123419466+Amrit478@users.noreply.github.com>
2025-04-08 07:51:48 -0400

    Update llIterative.java

--- Linkedlist/llIterative.java
+++ Linkedlist/llIterative.java
@@ -1,5 +1,15 @@
 /*In this file I will write few codes which will help me to remember what I learn last night when I did linked list*/
 
+/*This is where I have to use other variable because i will change the original head if i use the head function and it is not good. I want to perform a certain action
+based on given head. However, we do not want to change the main variable head, so we use different vairable and we can keep the original head safe and than return the
+answers based on required coding question
+
+Why we use different variable however in recursion we can use head
+
+Because in recursion we use call stack for each function so each function has its own call stack and its own value. However, we cannot do that in iteration because if we 
+use the same variable in iteration than it will change or modify the values given in the main class or variable, so in order to preserve the main linked list head we use
+or assign a different variable to head, so we can use that variable to perform the function. Now i will do coding for each function. */
+
 //Writing first void method as a iteration
 public void 
 
