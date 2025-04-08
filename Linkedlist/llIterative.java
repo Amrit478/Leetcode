34fbb1046cc0a58325b5120bf39fd15f9147bd2f
AMRITRAJ S DHILLON <123419466+Amrit478@users.noreply.github.com>
2025-04-08 06:40:10 -0400

    Create llIterative.java

---
+++ Linkedlist/llIterative.java
@@ -0,0 +1,8 @@
+/*In this file I will write few codes which will help me to remember what I learn last night when I did linked list*/
+
+//Writing first void method as a iteration
+public void 
+
+//Writing int method where it will return the linked list
+
+//Writing the method where I delete one linked list and than return the other linked list
