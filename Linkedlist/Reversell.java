1a8ffa905c953f578c50229ef365b7bc0af62499
AMRITRAJ S DHILLON <123419466+Amrit478@users.noreply.github.com>
2025-04-19 15:55:50 -0400

    Update Reversell.java

--- Linkedlist/Reversell.java
+++ Linkedlist/Reversell.java
@@ -5,6 +5,7 @@ Output: head: 4 -> 3 -> 2 -> 1 -> NULL
 Explanation: Reversed Linked List:
 Input: head: 1 -> 2 -> 3 -> 4 -> 5 -> NULL
 Output: head: 5 -> 4 -> 3 -> 2 -> 1 -> NULL*/
+  /*We will also reverse using the recursion*/
 
 //Node class will be here
 class Node {
@@ -16,8 +17,34 @@ Node(int data) {
 }
 }
 
-/*We will reverse using Stacks for the Iteration*/
-public
+/*We will reverse using Stacks for the Iteration because when we use stacks than the first thing is going this linked list into stack which will go first and when we pop the stack than
+it will come out last in out first which makes it reversing the linked list quicker
+WE ARE USING THE LIFO WHICH IS BASED ON STACKS*/
+class GfG {
+  	
+// Function to reverse the linked list and than store into stacks and than pushing Node into stacks
+  static Node reverselist(Node node);
+
+Stack<Integer> amrit = new Stack<>();
+  Node current = head;
+    while(current != null) {
+      amrit.push(current);
+      current = current.next;
+  }
+
+//This function will make the head the current node 
+if(!stack.isEmpty()) {
+  head = stack.pop();
+  current = head;
+}
+
+//Pop each function of the stack and return the reversal of the linked list
+while(!stack.isEmpty()) {
+  //I am using current.next here because I have already given head current here
+  current.next = current.pop();
+  current = current.next;
+}
+return head;
 
 
   // This function prints the contents 
@@ -50,7 +77,7 @@ public
     }
 }
 
-  /*We will also reverse using the recursion*/
+
 
 
 
