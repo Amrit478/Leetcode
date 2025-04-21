ee8a9414ee57bdd623637ed559e12ac931bddf62
AMRITRAJ S DHILLON <123419466+Amrit478@users.noreply.github.com>
2025-04-21 10:08:18 -0400

    Create start.java

---
+++ Linkedlist/Problemforleetcode/start.java
@@ -0,0 +1,13 @@
+/*Given a singly linked list and a key, the task is to delete the last occurrence of that key in the linked list.
+
+Examples: 
+Input: head: 1 -> 2 -> 3 ->1  -> 4 -> NULL, key = 1 
+Output: 1 -> 2 -> 3 -> 4 -> NULL
+*/
+
+/*The idea in the question is that I have to delete the key occurance which is happen in the last of linked list and it is singly linked list so I have to travese the 
+whole singly linked list and find and store the number where the last time key was found and than delete that key*/
+
+/*ALGORITHM WITH STEPS ARE GIVEN HERE 
+
+1. */
