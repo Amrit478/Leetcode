76e692ea39c839cf2f7b568a55f05845e0c97244
AMRITRAJ S DHILLON <123419466+Amrit478@users.noreply.github.com>
2025-04-21 20:25:07 -0400

    It will be first question done at 6 am tommrow

---
+++ Arrays/Question/triplet.java
@@ -0,0 +1,15 @@
+/*Given an integer array, find a maximum product of a triplet in the array.
+
+Examples: 
+Input:  arr[ ] = [10, 3, 5, 6, 20]
+Output: 1200
+Explanation: Multiplication of 10, 6 and 20
+Input:  arr[ ] =  [-10, -3, -5, -6, -20]
+Output: -90
+Input: arr[ ] =  [1, -4, 3, -6, 7, 0]
+Output: 168*/
+
+/*I have to find the top three numbers in each array and than multiply them and return thier sum*/
+
+/*ALGORITHM WITH THE GIVEN STEPS IS HERE
+*/
