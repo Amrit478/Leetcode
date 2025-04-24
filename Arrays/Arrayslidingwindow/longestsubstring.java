eab162b27c3ee13ebc3490cb806662081ce21221
AMRITRAJ S DHILLON <123419466+Amrit478@users.noreply.github.com>
2025-04-24 18:30:17 -0400

    Create longestsubstring.java

---
+++ Arrays/Arrayslidingwindow/longestsubstring.java
@@ -0,0 +1,13 @@
+/*Question:
+Given a string s and an integer k, return the length of the longest substring that contains at most k distinct characters.
+Input: s = "eceba", k = 2  
+Output: 3  
+Explanation: The substring is "ece" with length 3.
+Input: s = "aa", k = 1  
+Output: 2  
+Explanation: The entire string "aa" has only one distinct character.
+🔹 Constraints:
+1 ≤ k ≤ 26 here it is O(2^n) and it will be solved using recursion, backtracking or bit manipulation
+s.length ≤ 10⁵
+s consists of lowercase English letters*/
+
