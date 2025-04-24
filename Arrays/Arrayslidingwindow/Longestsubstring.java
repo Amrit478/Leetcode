71e36f46505ce0b87b1b98ae48eef0423cfb001b
AMRITRAJ S DHILLON <123419466+Amrit478@users.noreply.github.com>
2025-04-24 18:32:18 -0400

    Create Longestsubstring.java

---
+++ Arrays/Arrayslidingwindow/Longestsubstring.java
@@ -0,0 +1,14 @@
+/*Medium/Hard: Longest Substring Without Repeating Characters
+Question:
+
+Given a string s, find the length of the longest substring without repeating characters.
+Input: s = "abcabcbb"  
+Output: 3  
+Explanation: The answer is "abc", with the length of 3.
+Input: s = "bbbbb"  
+Output: 1  
+Explanation: The answer is "b", with the length of 1.
+🔹 Constraints:
+0 ≤ s.length ≤ 5 * 10⁴
+
+s consists of English letters, digits, symbols, and spaces.*/
