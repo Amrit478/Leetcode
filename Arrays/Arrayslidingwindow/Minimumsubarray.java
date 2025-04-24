891ae9efbeecf79fc97b6dbf95412310b829028e
AMRITRAJ S DHILLON <123419466+Amrit478@users.noreply.github.com>
2025-04-24 18:31:22 -0400

    Create Minimumsubarray.java

---
+++ Arrays/Arrayslidingwindow/Minimumsubarray.java
@@ -0,0 +1,11 @@
+/*Minimum Size Subarray Sum
+Question:
+Given an array of positive integers nums and a positive integer target, return the minimum length of a subarray whose sum is greater than or equal to target. If no such subarray exists, return 0.
+Input: target = 7, nums = [2,3,1,2,4,3]  
+Output: 2  
+Explanation: The subarray [4,3] has the minimal length under the problem constraint.
+🔹 Constraints:
+1 ≤ target ≤ 10⁹
+1 ≤ nums.length ≤ 10⁵
+1 ≤ nums[i] ≤ 10⁴
+*/
