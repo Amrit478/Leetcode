243ad41d79b2621e4bcf4c3836bbaf59a7f8da32
AMRITRAJ S DHILLON <123419466+Amrit478@users.noreply.github.com>
2025-04-25 06:16:03 -0400

    Create deletemiddle.java

---
+++ Linkedlist/Problemforleetcode/deletemiddle.java
@@ -0,0 +1,9 @@
+/*Given a singly linked list, the task is to delete the middle node of the list.
+If the list contains an even number of nodes, there will be two middle nodes. In this case, delete the second middle node.
+If the linked list consists of only one node, then return NULL.
+Example:
+Input: LinkedList: 1->2->3->4->5
+Output: 1->2->4->5
+Input: LinkedList: 2->4->6->7->5->1
+Output: 2->4->6->5->1
+*/
