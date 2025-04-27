af7269874b2b0a87871dd450b87310201035328e
AMRITRAJ S DHILLON <123419466+Amrit478@users.noreply.github.com>
2025-04-27 12:15:14 -0400

    Create removeduplicate.java

---
+++ Linkedlist/removeduplicate.java
@@ -0,0 +1,7 @@
+/*Given a linked list sorted in non-decreasing order. Return the list by deleting the duplicate nodes from the list. The returned list should also be in non-decreasing order.
+
+Example:
+    Input : Linked List = 11->11->11->21->43->43->60
+    Output : 11->21->43->60*/
+
+/**/
