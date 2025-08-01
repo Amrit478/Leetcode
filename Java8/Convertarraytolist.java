ba50c110237e0c14ecb44cd34cb58b5a3af47f8e
AMRITRAJ S DHILLON <123419466+Amrit478@users.noreply.github.com>
2025-07-31 20:25:05 -0400

    Create Convertarraytolist.java

---
+++ Java8/Convertarraytolist.java
@@ -0,0 +1,16 @@
+/*1.1 Using Arrays.asList() method to convert from an Array to a List*/
+
+Integer intergerArray[] = new int{2,3,45,6,546,67,,76,5,4,3,};
+List<Integer> cList = Array.asList(integerArray);
+ System.out.println(cList);
+
+/*1.3 Using Java 8’s Arrays.stream() method with a Collector*/
+
+
+
+
+
+
+
+
+/**/
